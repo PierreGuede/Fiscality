72ac10893057aeb9f3a7fb9082362c607cd3dd67
devalade <aladecharaf23@gmail.com>
2022-11-07 11:36:09 +0100

    feat: frais financier and commission view

---
+++ app/Http/Livewire/OtherReintegration/Details.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Livewire\OtherReintegration;
+
+use Livewire\Component;
+
+class Details extends Component
+{
+    public function render()
+    {
+        return view('livewire.other-reintegration.details');
+    }
+
+
+}
---
+++ resources/views/livewire/other-reintegration/create-commission-on-purchases.blade.php
@@ -0,0 +1,90 @@
+<div
+    x-show="$wire.open_a_side"
+    x-cloak
+    x-transition
+    class="fixed bg-white/30 backdrop-blur-sm w-full  top-0 left-0  h-screen z-50  ">
+    <button wire:click="closeASide"
+            class="absolute outline-none group top-2 -translate-x-full  left-[calc((100%/12)-0.2rem)] bg-white rounded-full p-2 z-50 ">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+             class="w-6 h-6 group-hover:rotate-45 duration-300 hover:transition-all ">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+        </svg>
+    </button>
+    <div x-data="{ lib_condition_response: 'yes', delay_condition_response: 'yes' }"
+         class="relative overflow-y-auto w-11/12 bg-white h-full ml-auto  px-12">
+        <h2 class="text-2xl font-bold text-gray-7002 py-8">Commission sur achats (facturés par des résidents et des
+            étrangers)</h2>
+
+        <div class="">
+            <div class="flex gap-x-3">
+                <p>2.</p>
+                <p>Conditions applicables à tous les intérêts</p>
+            </div>
+            <div class=" ml-6 mt-4 space-y-4">
+                <div class="grid grid-cols-12 gap-x-2">
+                    <h5 class="py-1 text-sm font-semibold text-gray-700 col-span-1">Compte </h5>
+                    <h5 class="py-1 text-sm font-semibold text-gray-700 col-span-3">Intitulé </h5>
+                    <h5 class="py-1 text-sm font-semibold text-gray-700 col-span-2">Total des achats </h5>
+                    <h5 class="py-1 text-sm font-semibold text-gray-700 col-span-2">Montant des commissions</h5>
+                    <h5 class="py-1 text-sm font-semibold text-gray-700 col-span-2">Limite de déduction</h5>
+                    <h5 class="py-1 text-sm font-semibold text-gray-700 col-span-2">Montant non déductible</h5>
+
+
+                </div>
+
+
+                @foreach($inputs as  $key => $value)
+                    <div class="flex gap-x-2 items-center ">
+
+                        <div class="grid grid-cols-12 gap-x-2">
+                            <div class="col-span-1">
+                                <x-input type="number" label="" id="delay_condition" name=""
+                                         value="{{ old('delay_condition') }}" class="block w-full" required autofocus/>
+                            </div>
+
+
+                            <div class="col-span-3">
+                                <x-input type="number" label="" id="delay_condition" name=""
+                                         value="{{ old('delay_condition') }}" class="block w-full" required autofocus/>
+                            </div>
+
+                            <div class="col-span-2">
+                                <x-input type="number" label="" id="delay_condition" name=""
+                                         value="{{ old('delay_condition') }}" class="block w-full" required autofocus/>
+                            </div>
+
+                            <div class="col-span-2">
+                                <x-input type="number" label="" id="delay_condition" name=""
+                                         value="{{ old('delay_condition') }}" class="block w-full" required autofocus/>
+                            </div>
+
+                            <div class="col-span-2">
+                                <x-input type="number" label="" id="delay_condition" name=""
+                                         value="{{ old('delay_condition') }}" class="block w-full" required autofocus/>
+                            </div>
+
+                            <div class="col-span-2">
+                                <x-input type="number" label="" id="delay_condition" name=""
+                                         value="{{ old('delay_condition') }}" class="block w-full" required autofocus/>
+                            </div>
+
+                        </div>
+
+                        <div class="col-span-1">
+                            <button class=" hover:bg-red-100 p-1.5 rounded-md ">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     stroke-width="1.5"
+                                     stroke="currentColor"
+                                     class="w-4 h-4 stroke-2 stroke-red-500    ">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"/>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                @endforeach
+
+            </div>
+        </div>
+    </div>
+</div>
